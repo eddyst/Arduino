f79ca6b6e500a3b672c3cd9f2930bcf4598b31c1
Eddy Steier <est.git@online.de>
2013-05-27 22:14:31 +0200

    Datenpunkte abgeglichen
    VorlaufMieter und VorlaufSteier entfernt - sollten ja gleich VorlaufGemischt sein
    
    BrennerAn entfernt - ergibt sich aus Leistung

--- _00_Global.ino
+++ _00_Global.ino
@@ -16,11 +16,9 @@ typedef struct {
 #define _ThermeBrennerLeistung       9
 #define _HKVorlauf                  10
 #define _HKVorlaufGemischt          11
-#define _HKVorlaufMieter            12
-#define _HKVorlaufSteier            13
-#define _HKRuecklauf                14
-#define _HKRuecklaufMieter          15
-#define _HKRuecklaufSteier          16
+#define _HKRuecklauf                12
+#define _HKRuecklaufMieter          13
+#define _HKRuecklaufSteier          14
 
 prog_char FHEM_ThermeKesselTempSoll       [] PROGMEM =  "ThermeKesselTempSoll";
 prog_char FHEM_ThermeKesselTempIst        [] PROGMEM =  "ThermeKesselTempIst";
@@ -34,35 +32,33 @@ prog_char FHEM_ThermeUmschaltventilTaster [] PROGMEM =  "ThermeUmschaltventilTas
 prog_char FHEM_ThermeBrennerLeistung      [] PROGMEM =  "ThermeBrennerLeistung";
 prog_char FHEM_HKVorlauf                  [] PROGMEM =  "HKVorlauf";
 prog_char FHEM_HKVorlaufGemischt          [] PROGMEM =  "HKVorlaufGemischt";
-prog_char FHEM_HKVorlaufMieter            [] PROGMEM =  "HKVorlaufMieter";
-prog_char FHEM_HKVorlaufSteier            [] PROGMEM =  "HKVorlaufSteier";
 prog_char FHEM_HKRuecklauf                [] PROGMEM =  "HKRuecklauf";
 prog_char FHEM_HKRuecklaufMieter          [] PROGMEM =  "HKRuecklaufMieter";
 prog_char FHEM_HKRuecklaufSteier          [] PROGMEM =  "HKRuecklaufSteier";
 
 #define ValueUnknown -10000
-data Values[] = {{ 0, ValueUnknown, FHEM_ThermeKesselTempSoll       }, // 1
+data Values[] = {{ 0, ValueUnknown, FHEM_ThermeKesselTempSoll       }, // 0
                  { 0, ValueUnknown, FHEM_ThermeKesselTempIst        }, 
                  { 0, ValueUnknown, FHEM_ThermeVorlaufTempSoll      }, 
                  { 0, ValueUnknown, FHEM_ThermeRuecklaufTemp        }, 
-                 { 0, ValueUnknown, FHEM_ThermeSpeicherTemp         }, // 5
-                 { 0, ValueUnknown, FHEM_ThermeAussenTemp           }, 
+                 { 0, ValueUnknown, FHEM_ThermeSpeicherTemp         }, 
+                 { 0, ValueUnknown, FHEM_ThermeAussenTemp           }, // 5
                  { 0, ValueUnknown, FHEM_ThermeBetriebsart          }, 
                  { 0, ValueUnknown, FHEM_ThermeUmschaltventilOpto   }, 
                  { 0, ValueUnknown, FHEM_ThermeUmschaltventilTaster }, 
-                 { 0, ValueUnknown, FHEM_ThermeBrennerLeistung      }, //10
-                            { 0, ValueUnknown, FHEM_HKVorlauf                  }, 
-                            { 0, ValueUnknown, FHEM_HKVorlaufGemischt          }, 
-                            { 0, ValueUnknown, FHEM_HKVorlaufMieter            }, 
-                            { 0, ValueUnknown, FHEM_HKVorlaufSteier            }, //15
-                            { 0, ValueUnknown, FHEM_HKRuecklauf                }, 
-                            { 0, ValueUnknown, FHEM_HKRuecklaufMieter          }, 
-                            { 0, ValueUnknown, FHEM_HKRuecklaufSteier          }  
+                 { 0, ValueUnknown, FHEM_ThermeBrennerLeistung      }, 
+                 { 0, ValueUnknown, FHEM_HKVorlauf                  }, //10
+                 { 0, ValueUnknown, FHEM_HKVorlaufGemischt          }, 
+                 { 0, ValueUnknown, FHEM_HKRuecklauf                }, 
+                 { 0, ValueUnknown, FHEM_HKRuecklaufMieter          }, 
+                 { 0, ValueUnknown, FHEM_HKRuecklaufSteier          }  
                 };
 
-uint8_t  owAddr[][9] = { { 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 10},
-                         { 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 11},
-                         { 0x03, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 12} };
+uint8_t  owAddr[][9] = { { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 10},
+                         { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 11},
+                         { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 12},
+                         { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 13},
+                         { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 14} };
 
 uint8_t  ThermeVorlaufTempVorgabe = 50; //ToDo: Abfragen und bei Änderung KesselSollTempVorgabeRechnen();
 uint8_t  HKSollTempVorgabe     = 45; //ToDo: Abfragen
